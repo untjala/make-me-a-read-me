4330c2faf3cb70b2ce4e19bde769d0a06822456f
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-14 15:29:54 -0400

    filled questions array

--- index.js
+++ index.js
@@ -2,7 +2,38 @@
 const fs = require('fs');
 const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What does your project do?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Describe the installation process for your project: ',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'List any contributors to your project: ',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'What tests did you perform for your project?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What liscense is your project under?',
+        name: 'license',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
